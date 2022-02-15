ef741624529b58856d785cb67aafee86a0860f0e
alx9865 <94532724+alx9865@users.noreply.github.com>
2022-02-15 15:17:20 +0100

    push

--- app-module_reactjs/src/App.js
+++ app-module_reactjs/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Row, Col } from "react-bootstrap";
+import {} from "./components/jouer.jsx";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Jeu du pendu</p>
+        <Row>
+          <button variant="dark" type="submit" to="/components/jouer.jsx">
+            Jouer
+          </button>
+          <button>Liste</button>
+        </Row>
       </header>
     </div>
   );
